acda8d4ce628368ef06b772c111a7cd3b692f139
nedot1 <tanja.nedovic@students.bfh.ch>
2017-11-10 12:23:54 +0100

    Change rootPage from LoginPage to HomePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
 
-  rootPage: any = LoginPage;
+  rootPage: any = HomePage;
 
   pages: Array<{ title: string, component: any, icon: string }>;
 
