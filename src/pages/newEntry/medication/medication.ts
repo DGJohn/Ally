140460b3418bb22fea344fcec2bf25fd6f905e6c
Roberto <edisonroberto.tangarifebonilla@students.bfh.ch>
2018-01-05 16:07:10 +0100

    Medication Page modified

--- src/pages/newEntry/medication/medication.ts
+++ src/pages/newEntry/medication/medication.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { TotalPointsPage } from '../totalPoints/totalPoints';
 import { HomePage } from '../../../pages/home/home';
@@ -9,8 +9,26 @@ import { HomePage } from '../../../pages/home/home';
 })
 export class MedicationPage {
 
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+  private isDisabled:boolean = true;
+  private questionMedication:any={};
+  private questionMedicationWhy:any={};
+
+
+  constructor(private zone:NgZone, public navCtrl: NavController, private toastCtrl: ToastController) {
+    this.questionMedication = true;
+    this.questionMedicationWhy = true;
+    this.isDisabled = false;
   }
+
+  change(val: any) {
+    this.zone.run(()=>{
+      if(val)
+        this.isDisabled = false;
+      else
+        this.isDisabled = true;
+        console.log(this.isDisabled);
+    })
+ }
 
   pushTotalPointsPage() {
     this.navCtrl.push(TotalPointsPage)
