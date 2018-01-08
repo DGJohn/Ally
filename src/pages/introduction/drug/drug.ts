d544301df7016fe3f8e7519c0ccf63f6815aff54
Roberto <edisonroberto.tangarifebonilla@students.bfh.ch>
2018-01-08 17:16:18 +0100

    Pages allergy and drug modified

--- src/pages/introduction/drug/drug.ts
+++ src/pages/introduction/drug/drug.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { ReminderTimePage } from '../remindertime/remindertime';
 import { HomePage } from '../../../pages/home/home';
 
@@ -9,8 +9,56 @@ import { HomePage } from '../../../pages/home/home';
 })
 export class DrugPage {
 
-  constructor(public navCtrl: NavController) {
+  public isDisabled: boolean = true;
+  useMedication: any = {};
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.useMedication = false;
   }
+
+  change(val: any) {
+   if (val)
+     this.openCheckPopUp()
+   }
+
+   openCheckPopUp(){
+       let alert = this.alertCtrl.create();
+       alert.setTitle('tägliche Dosierung');
+
+       alert.addInput({
+         type: 'checkbox',
+         label: 'morgens',
+         value: 'morgens',
+         checked: true
+       });
+
+       alert.addInput({
+         type: 'checkbox',
+         label: 'mittags',
+         value: 'mittags'
+       });
+
+       alert.addInput({
+         type: 'checkbox',
+         label: 'abends',
+         value: 'abends'
+       });
+
+       alert.addInput({
+         type: 'checkbox',
+         label: 'nachts',
+         value: 'nachts'
+       });
+
+       alert.addButton('Abbrechen');
+       alert.addButton({
+         text: 'Ok',
+         handler: (data: any) => {
+           console.log('Radio data:', data);
+         }
+       });
+       alert.present();
+   }
 
   pushReminderTimePage() {
     this.navCtrl.push(ReminderTimePage)
